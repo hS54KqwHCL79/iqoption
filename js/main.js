dd962ea4ed8a54a01d6c89c7287122a15f1e5ba8
Andrew Muliar <andrewmuliar7@gmail.com>
2018-02-09 18:38:29 +0200

    upgrd

--- js/main.js
+++ js/main.js
@@ -243,7 +243,6 @@ function revod(result, currency, crypt)
 
  $(document).ready(function()
  {
-
   $.ajax({
         type:     "GET",
 		cache:    false,
@@ -305,3 +304,28 @@ function revod(result, currency, crypt)
    $(this).children().css("opacity","1")
   })
  })
+ // Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("terms");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
